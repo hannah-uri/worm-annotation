2d41722c1c8091d887ebd4398b63d63ef09a67f0
hannah-uri <56790928+hannah-uri@users.noreply.github.com>
2019-11-12 20:31:13 +0000

    Add files via upload

---
+++ maker-check.sh
@@ -0,0 +1,12 @@
+#tests maker on womrbase data
+
+mkdir ref
+wget -P ref/ "ftp://ftp.wormbase.org/pub/wormbase/releases/WS274/species/c_elegans/PRJEB28388/c_elegans.PRJEB28388.WS274.genomic.fa.gz"
+wget -P ref/ "ftp://ftp.wormbase.org/pub/wormbase/releases/WS274/species/c_elegans/PRJEB28388/c_elegans.PRJEB28388.WS274.mRNA_transcripts.fa.gz"
+gunzip ref/c_elegans.PRJEB28388.WS274.genomic.fa.gz
+gunzip ref/c_elegans.PRJEB28388.WS274.mRNA_transcripts.fa.gz
+
+maker -CTL
+cat maker_opts.ctl | sed 's/^genome=.*$/genome=ref\/c_elegans.PRJEB28388.WS274.genomic.fa /' | sed 's/^est=.*$/est=ref\/c_elegans.PRJEB28388.WS274.mRNA_transcripts.fa /' | sed 's/^model_org=.*$/model_org= /' > maker_opts.ctl
+
+maker
