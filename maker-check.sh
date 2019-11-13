b485a0841522695f1d013459484509e4a191f545
hannah-uri <56790928+hannah-uri@users.noreply.github.com>
2019-11-13 10:10:05 +0000

    Update maker-check.sh

--- maker-check.sh
+++ maker-check.sh
@@ -1,4 +1,5 @@
-#tests maker on womrbase data
+#!/bin/sh
+#tests maker on wormbase data
 
 mkdir ref
 wget -P ref/ "ftp://ftp.wormbase.org/pub/wormbase/releases/WS274/species/c_elegans/PRJEB28388/c_elegans.PRJEB28388.WS274.genomic.fa.gz"
@@ -7,6 +8,7 @@ gunzip ref/c_elegans.PRJEB28388.WS274.genomic.fa.gz
 gunzip ref/c_elegans.PRJEB28388.WS274.mRNA_transcripts.fa.gz
 
 maker -CTL
-cat maker_opts.ctl | sed 's/^genome=.*$/genome=ref\/c_elegans.PRJEB28388.WS274.genomic.fa /' | sed 's/^est=.*$/est=ref\/c_elegans.PRJEB28388.WS274.mRNA_transcripts.fa /' | sed 's/^model_org=.*$/model_org= /' > maker_opts.ctl
+cat maker_opts.ctl | sed 's/^genome=.*$/genome=ref\/c_elegans.PRJEB28388.WS274.genomic.fa /' | sed 's/^est=.*$/est=ref\/c_elegans.PRJEB28388.WS274.mRNA_transcripts.fa /' | sed 's/^model_org=.*$/model_org= /' > maker_opts.ctl.2
+mv maker_opts.ctl.2 maker_opts.ctl
 
 maker
