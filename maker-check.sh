39532e733fa5836e7bf231606333f51c0f17902a
hannah-uri <56790928+hannah-uri@users.noreply.github.com>
2019-11-13 13:15:37 +0000

    Update maker-check.sh

--- maker-check.sh
+++ maker-check.sh
@@ -4,11 +4,11 @@
 mkdir ref
 wget -P ref/ "ftp://ftp.wormbase.org/pub/wormbase/releases/WS270/species/c_elegans/PRJEB28388/c_elegans.PRJEB28388.WS270.genomic.fa.gz"
 wget -P ref/ "ftp://ftp.wormbase.org/pub/wormbase/releases/WS270/species/c_elegans/PRJEB28388/c_elegans.PRJEB28388.WS270.mRNA_transcripts.fa.gz"
-gunzip ref/c_elegans.PRJEB28388.WS274.genomic.fa.gz
-gunzip ref/c_elegans.PRJEB28388.WS274.mRNA_transcripts.fa.gz
+gunzip ref/c_elegans.PRJEB28388.WS270.genomic.fa.gz
+gunzip ref/c_elegans.PRJEB28388.WS270.mRNA_transcripts.fa.gz
 
 maker -CTL
-cat maker_opts.ctl | sed 's/^genome=.*$/genome=ref\/c_elegans.PRJEB28388.WS274.genomic.fa /' | sed 's/^est=.*$/est=ref\/c_elegans.PRJEB28388.WS274.mRNA_transcripts.fa /' | sed 's/^model_org=.*$/model_org= /' > maker_opts.ctl.2
+cat maker_opts.ctl | sed 's/^genome=.*$/genome=ref\/c_elegans.PRJEB28388.WS270.genomic.fa /' | sed 's/^est=.*$/est=ref\/c_elegans.PRJEB28388.WS270.mRNA_transcripts.fa /' | sed 's/^model_org=.*$/model_org= /' > maker_opts.ctl.2
 mv maker_opts.ctl.2 maker_opts.ctl
 
 maker
